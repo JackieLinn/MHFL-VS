8455d013731bc198654db85aaf6da01ca90027f5
JackieLinn <linxy20040304@163.com>
2026-01-31 15:39:22 +0800

    feat: Overall project basic configuration

---
+++ mhfl-server/src/test/java/ynu/jackielinn/server/MhflServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ynu.jackielinn.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhflServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
