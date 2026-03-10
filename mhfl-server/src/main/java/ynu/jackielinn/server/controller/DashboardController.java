2a6825f2c94f4e0dee3ac9e8641081842d0f3d58
JackieLinn <linxy20040304@163.com>
2026-03-10 21:15:52 +0800

    feat: Build the backend interface code related to the Dashboard

---
+++ mhfl-server/src/main/java/ynu/jackielinn/server/controller/DashboardController.java
@@ -0,0 +1,16 @@
+package ynu.jackielinn.server.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ynu.jackielinn.server.service.DashboardService;
+
+@RestController
+@RequestMapping("/api/system")
+@Tag(name = "仪表盘 Dashboard 接口", description = "仪表盘 Dashboard 操作相关接口")
+public class DashboardController {
+
+    @Resource
+    private DashboardService dashboardService;
+}
---
+++ mhfl-server/src/main/java/ynu/jackielinn/server/service/impl/DashboardServiceImpl.java
@@ -0,0 +1,8 @@
+package ynu.jackielinn.server.service.impl;
+
+import org.springframework.stereotype.Service;
+import ynu.jackielinn.server.service.DashboardService;
+
+@Service
+public class DashboardServiceImpl implements DashboardService {
+}
