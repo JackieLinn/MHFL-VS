1aa222ff0139cbd816639a173622b40f47352254
JackieLinn <linxy20040304@163.com>
2026-03-22 22:53:25 +0800

    test: Improve controller related tests

--- mhfl-server/src/test/java/ynu/jackielinn/server/MhflServerApplicationTests.java
+++ mhfl-server/src/test/java/ynu/jackielinn/server/MhflServerApplicationTests.java
@@ -3,7 +3,7 @@ package ynu.jackielinn.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.task.scheduling.enabled=false")
 class MhflServerApplicationTests {
 
     @Test
